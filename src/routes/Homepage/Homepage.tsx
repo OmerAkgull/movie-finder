dde846402adf581c8f5028dcf9a24b1227157149
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-03-08 17:41:39 +0300

    Fetch movies via useEffect

--- src/routes/Homepage/Homepage.tsx
+++ src/routes/Homepage/Homepage.tsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+import classes from "./Homepage.module.css"
 
 const Homepage = () => {
+
+  const [data, setData] = useState("");
+
+  async function getMovies() {
+    const data = await fetch(`http://www.omdbapi.com/?apikey=f1ec40aa&i=tt1285016`)
+    const json = await data.json()
+    setData(json);
+  }
+
+  console.log(data);
+
+  useEffect(() => {
+    getMovies();
+  }, [])
+
   return (
-    <div>Homepage</div>
+    <>
+      <div className={classes.appContainer}>
+      <h1 className={classes.header}> <img className={classes.omdbLogo} src="/omdb-logo.svg"></img> MDB MOVIE FINDER</h1>
+      </div>
+    </>
   )
 }
 
