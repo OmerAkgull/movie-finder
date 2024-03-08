218450d9a92bdc3b715bd68ee6722f95cd233a6f
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-03-08 16:49:44 +0300

    Create routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from './routes/Homepage';
+import Movie from './routes/Movie';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage/>,
+  },
+  {
+    path: "movie/:movieId",
+    element: <Movie/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
